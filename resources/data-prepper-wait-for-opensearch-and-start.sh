eebecac84e2c1d4a3b1ee19dba068916db163808
lphaniKumar <lphanikumar@yahoo.co.in>
2022-11-01 13:16:36 +0100

    commit2
    commit2

--- resources/data-prepper-wait-for-opensearch-and-start.sh
+++ resources/data-prepper-wait-for-opensearch-and-start.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-until [[ $(curl --write-out %{http_code} --output /dev/null --silent --head --fail https://node-0.example.com:9200 -u admin:admin --insecure) == 200 ]]; do
+until [[ $(curl --write-out %{http_code} --output /dev/null --silent --head --fail https://vpc-aosstraceanalytics-ihhshiknaitdndyqjow5m3wv5u.us-east-1.es.amazonaws.com:443 -u aosadmin:Passw0rd1! --insecure)) == 200 ]]; do
   echo "Waiting for OpenSearch to be ready"
   sleep 1
 done
